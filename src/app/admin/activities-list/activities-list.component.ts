99614496711aa5f310e8a389c5d70f581b07c852
yassineghzala <ghzalayassin@gmail.com>
2023-11-15 22:06:30 +0100

    dashboard almost finished: missing some details and design

---
+++ src/app/admin/activities-list/activities-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-activities-list',
+  templateUrl: './activities-list.component.html',
+  styleUrls: ['./activities-list.component.css']
+})
+export class ActivitiesListComponent {
+
+}
